71652dc9fd8d6a19ca963be52ab1cdfa8837c358
julianafrego <julianafgomes@aluno.uespi.br>
2026-08-29 15:59:04 -0300

    feat: dynamically add and customize books

--- src/app/features/home/components/notebook-list/book-card/book-card.ts
+++ src/app/features/home/components/notebook-list/book-card/book-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Book } from '../../../../models/book';
 
 @Component({
   imports: [],
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './book-card.css',
   templateUrl: './book-card.html',
 })
-export class BookCard {}
+export class BookCard {
+  book = input.required<Book>();
+
+  get spineColor(): string {
+    return this.darkenColor(this.book().coverColor, 15);
+  }
+
+  private darkenColor(hex: string, amount: number): string{
+      // O 16 força a conversão de hexadecimal para número decimal (Base 10).
+    const num = parseInt(hex.replace("#", ""), 16);
+
+    // const r calculates the red value of the color in the range 0-255.
+    const r = Math.max(0, (num >> 16) - amount);
+    // const g calculates the green value of the color in the range 0-255.
+    const g = Math.max(0, ((num >> 8) & 0xff) - amount);
+    // const b calculates the blue value of the color in the range 0-255.
+    const b = Math.max(0, (num & 0xff) - amount);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+}
+
--- src/app/features/home/components/notebook-list/notebook-list.ts
+++ src/app/features/home/components/notebook-list/notebook-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BookCard } from './book-card/book-card';
+import { Book } from '../../../models/book';
 
 @Component({
   imports: [BookCard],
@@ -7,4 +8,48 @@ import { BookCard } from './book-card/book-card';
   styleUrl: './notebook-list.css',
   templateUrl: './notebook-list.html',
 })
-export class NotebookList {}
+export class NotebookList {
+  books: Book[] = [
+    {
+      id: 1,
+      name: 'my first book',
+      coverColor: '#B83232'
+    },
+    {
+      id: 2,
+      name: 'my secondy book',
+      coverColor: '#6B7DB3'
+    },
+    {
+      id: 3,
+      name: 'my first book',
+      coverColor: '#B83232'
+    },
+    {
+      id: 4,
+      name: 'my secondy book',
+      coverColor: '#6B7DB3'
+    },
+    {
+      id: 5,
+      name: 'my first book',
+      coverColor: '#B83232'
+    },
+    {
+      id: 6,
+      name: 'my secondy book',
+      coverColor: '#6B7DB3'
+    },
+    {
+      id: 7,
+      name: 'my first book',
+      coverColor: '#B83232'
+    },
+    {
+      id: 8,
+      name: 'my secondy book',
+      coverColor: '#6B7DB3'
+    }
+
+  ];
+}
