653f5e5b65a152704badb7e26f57c2fbe4d18791
julianafrego <julianafgomes@aluno.uespi.br>
2026-09-01 14:56:37 -0300

    chore: separe topbar in bokki page

--- src/app/features/bokki/bokki.ts
+++ src/app/features/bokki/bokki.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { BookArea } from './components/book-area/book-area';
 import { Toolbar } from './components/toolbar/toolbar';
+import { Topbar } from './components/topbar/topbar';
 
 @Component({
-  imports: [BookArea, Toolbar],
+  imports: [Topbar, BookArea, Toolbar],
   selector: 'app-bokki',
   styleUrl: './bokki.css',
   templateUrl: './bokki.html',
