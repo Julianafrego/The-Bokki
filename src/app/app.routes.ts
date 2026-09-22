36b2ef2fd7f8a49ec3cd5b7c9535c4df15fc38c9
julianafrego <julianafgomes@aluno.uespi.br>
2026-09-22 18:18:54 -0300

    feat: Initialize home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Home } from './features/home/home';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Home
+  }
+];
